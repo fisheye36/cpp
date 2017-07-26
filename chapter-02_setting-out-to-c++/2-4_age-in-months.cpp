3fd77ad948310a85b044d26d635c420a07db17f0
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-07-26 15:28:17 +0200

    Add 2-4_age-in-months.cpp

---
+++ chapter-02_setting-out-to-c++/2-4_age-in-months.cpp
@@ -0,0 +1,33 @@
+// exercise 2.4
+
+/**
+ *
+ * Write a program that asks the user to enter his or her age. The program then should
+ * display the age in months:
+ *
+ * Enter your age: 29
+ * Your age in months is 348.
+ *
+ */
+
+#include <iostream>
+
+int ageInMonths(int);
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+
+    cout << "Enter your age: ";
+    int age;
+    cin >> age;
+    cout << "Your age in months is " << ageInMonths(age) << ".\n";
+
+    return 0;
+}
+
+int ageInMonths(int age)
+{
+    return age * 12;
+}
