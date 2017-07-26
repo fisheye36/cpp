47ba389b771242c6928a1383a3e2b2a93b515f90
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-07-26 15:23:35 +0200

    Add 2-2_furlongs-to-yards.cpp

---
+++ chapter-02_setting-out-to-c++/2-2_furlongs-to-yards.cpp
@@ -0,0 +1,30 @@
+// exercise 2.2
+
+/**
+ *
+ * Write a C++ program that asks for a distance in furlongs and converts it to yards.
+ * (One furlong is 220 yards.)
+ *
+ */
+
+#include <iostream>
+
+int furlongsToYards(int);
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+
+    cout << "Enter a distance in furlongs: ";
+    int furlongs;
+    cin >> furlongs;
+    cout << furlongs << " furlongs = " << furlongsToYards(furlongs) << " yards.\n";
+
+    return 0;
+}
+
+int furlongsToYards(int furlongs)
+{
+    return furlongs * 220;
+}
