74b16fb366eb954a5f301f91d2571fc4ecaac31c
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-07-26 15:30:46 +0200

    Add 2-5_temperature.cpp

---
+++ chapter-02_setting-out-to-c++/2-5_temperature.cpp
@@ -0,0 +1,39 @@
+// exercise 2.5
+
+/**
+ *
+ * Write a program that has main() call a user-defined function that takes a Celsius
+ * temperature value as an argument and then returns the equivalent Fahrenheit value.
+ * The program should request the Celsius value as input from the user and display
+ * the result, as shown in the following code:
+ *
+ * Please enter a Celsius value: 20
+ * 20 degrees Celsius is 68 degrees Fahrenheit.
+ *
+ * For reference, here is the formula for making the conversion:
+ * Fahrenheit = 1.8 * degrees Celsius + 32.0
+ *
+ */
+
+#include <iostream>
+
+double celsiusToFahrenheit(double);
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+
+    cout << "Please enter a Celsius value: ";
+    double celsius;
+    cin >> celsius;
+    cout << celsius << " degrees Celsius is " << celsiusToFahrenheit(celsius)
+         << " degrees Fahrenheit.\n";
+
+    return 0;
+}
+
+double celsiusToFahrenheit(double celsius)
+{
+    return 1.8 * celsius + 32.0;
+}
