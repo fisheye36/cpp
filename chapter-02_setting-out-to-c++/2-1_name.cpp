17f4315f0014167d7271697f5bf9f4236c439db1
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-07-26 15:22:44 +0200

    Add 2-1_name.cpp

---
+++ chapter-02_setting-out-to-c++/2-1_name.cpp
@@ -0,0 +1,21 @@
+// exercise 2.1
+
+/**
+ *
+ * Write a C++ program that displays your name and address (or if you value your
+ * privacy, a fictious name and address).
+ *
+ */
+
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+
+    cout << "Kamil Warchol\n";
+    cout << "Przyszlosci 15\n";
+    cout << "32-566 Alwernia\n";
+
+    return 0;
+}
