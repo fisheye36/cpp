e84067b057f82fe9e47c29576da7ab6adef45247
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-12-08 21:01:44 +0100

    Add 4-02_dessert.cpp

---
+++ chapter-04_compound-types/4-02_dessert.cpp
@@ -0,0 +1,29 @@
+// exercise 4.2
+
+/**
+ *
+ * Rewrite Listing 4.4, using the C++ string class instead of char arrays.
+ *
+ */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    using std::string;
+
+    cout << "Enter your name: ";
+    string name;
+    getline(cin, name);
+
+    cout << "Enter your favorite dessert: ";
+    string dessert;
+    getline(cin, dessert);
+
+    cout << "I have some delicious " << dessert << " for you, " << name << ".\n";
+
+    return 0;
+}
