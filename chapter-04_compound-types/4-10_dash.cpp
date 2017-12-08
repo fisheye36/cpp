37b1d98e34925d37f7bf98787c6efa40df10889b
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-12-09 00:13:35 +0100

    Add 4-10_dash.cpp

---
+++ chapter-04_compound-types/4-10_dash.cpp
@@ -0,0 +1,33 @@
+// exercise 4.10
+
+/**
+ *
+ * Write a program that requests the user to enter three times for the 40-yd dash (or
+ * 40-meter, if you prefer) and then displays the times and the average. Use an array
+ * object to hold the data. (Use a built-in array if array is not available.)
+ *
+ */
+
+#include <iostream>
+#include <array>
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+
+    const int Amount = 3;
+
+    cout << "Enter " << Amount << " times for the 40-meter dash in seconds:\n";
+    std::array<double, Amount> times;
+    cin >> times[0];
+    cin >> times[1];
+    cin >> times[2];
+
+    double averageTime = (times[0] + times[1] + times[2]) / Amount;
+    cout << "\nYour times are: "
+         << times[0] << " s, " << times[1] << " s, " << times[2] << " s.\n"
+         << "Your average time is " << averageTime << " s.\n";
+
+    return 0;
+}
