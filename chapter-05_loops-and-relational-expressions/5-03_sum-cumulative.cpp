9e5f00aa2bef7daf576894bb26f13d5e42dbf909
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-12-12 18:29:50 +0100

    Add 5-03_sum-cumulative.cpp

---
+++ chapter-05_loops-and-relational-expressions/5-03_sum-cumulative.cpp
@@ -0,0 +1,33 @@
+// exercise 5.3
+
+/**
+ *
+ * Write a program that asks the user to type in numbers. After each entry, the program
+ * should report the cumulative sum of the entries to date. The program should
+ * terminate when the user enters 0.
+ *
+ */
+
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+
+    cout << "Enter a number: ";
+    int number;
+    cin >> number;
+
+    long long sum = 0LL;
+    while (number)
+    {
+        sum += number;
+        cout << "The cumulative sum of the entries to date is " << sum << ".\n";
+
+        cout << "Enter another number (0 to quit): ";
+        cin >> number;
+    }
+
+    return 0;
+}
