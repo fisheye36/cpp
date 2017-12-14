3c5721af7c0b0cb44ceafc90d15e0a3cc023247c
Kamil Warchoł <kamil.warchol.93@gmail.com>
2017-12-14 22:51:45 +0100

    Add 5-08_words.cpp

---
+++ chapter-05_loops-and-relational-expressions/5-08_words.cpp
@@ -0,0 +1,41 @@
+// exercise 5.8
+
+/**
+ *
+ * Write a program that uses an array of char and a loop to read one word at a time
+ * until the word done is entered. The program should then report the number of
+ * words entered (not counting done). A sample run could look like this:
+ *
+ * Enter words (to stop, type the word done):
+ * anteater birthday category dumpster
+ * envy finagle geometry done for sure
+ * You entered a total of 7 words.
+ *
+ * You should include the cstring header file and use the strcmp() function to
+ * make the comparison test.
+ *
+ */
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using std::cout;
+
+    const int MaxSize = 40;
+    const char * ExitWord = "done";
+
+    cout << "Enter words (to stop, type the word done):\n";
+    char word[MaxSize];
+    std::cin >> word;
+
+    unsigned amount = 0u;
+    while (strcmp(word, ExitWord))
+    {
+        std::cin >> word;
+        amount++;
+    }
+
+    cout << "You entered a total of " << amount << " words.\n";
+}
